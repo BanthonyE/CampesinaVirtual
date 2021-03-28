0839c443ea6eaa319e8a4ad819696e919d457848
xdgonex <bendezucortezjoseluis@gmail.com>
2021-03-28 00:23:51 -0500

    Nombres y Descripciones de la pagina

--- Controllers/Roles.php
+++ Controllers/Roles.php
@@ -20,7 +20,7 @@ class Roles extends Controllers
 		$data['page_id'] = 3;
 		$data['page_tag'] = "Roles Usuario";
 		$data['page_name'] = "rol_usuario";
-		$data['page_title'] = "Roles Usuario <small> Tienda Virtual</small>";
+		$data['page_title'] = "Roles Usuario <small> Rondas Comuneras</small>";
 		$data['page_functions_js'] = "functions_roles.js";
 		$this->views->getView($this, "roles", $data);
 	}

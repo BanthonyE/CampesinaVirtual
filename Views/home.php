3a45b3d49eb62efd125730dd5a35f8733616db94
MARIOCV <mario006121@gmail.com>
2021-04-02 01:52:25 -0500

    carnet4.0

--- Views/home.php
+++ Views/home.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Mini Framework - AbelOSh</title>
+  <title>Mini Framework - DigitalTEAM</title>
 </head>
 <body>
-    <p>Mini Framework V1.0 - <a href="https://abelosh.com">www.abelosh.com</a></p>
+    <p>Mini Framework V1.0 - <a href="https://abelosh.com">www.DigitalTEAM.com</a></p>
 </body>
 </html>

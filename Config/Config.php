582743cbb882411181f72d33b321b379bd7f8244
AnthonyEO <anthony220e@gmail.com>
2021-04-03 14:52:22 -0500

    credenciales heroku

--- Config/Config.php
+++ Config/Config.php
@@ -2,17 +2,17 @@
 
 
 	
-	const BASE_URL = "http://localhost:90/Campesinavirtual";
+/* 	const BASE_URL = "http://localhost:90/Campesinavirtual";
 
 	date_default_timezone_set('America/Guatemala');
 	const DB_HOST = "localhost";
 	const DB_NAME = "db_campesinavirtual";
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8";
+	const DB_CHARSET = "utf8"; */
 
 
-/* 	const BASE_URL = "https://campesinavirtual.herokuapp.com";
+	const BASE_URL = "https://campesinavirtual.herokuapp.com";
 
 	date_default_timezone_set('America/Guatemala');
 
@@ -20,7 +20,7 @@
 	const DB_NAME = "qhrtzyzdmrx5j42u";
 	const DB_USER = "dnjmxhbd9vepxngn";
 	const DB_PASSWORD = "euouo3jyoy0o5vzj";
-	const DB_CHARSET = "utf8"; */
+	const DB_CHARSET = "utf8";
 
 
 //Deliminadores decimal y millar Ej. 24,1989.00

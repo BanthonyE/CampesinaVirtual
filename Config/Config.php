3c9db9ee5438395e365175019a6bb73e01061920
xdgonex <bendezucortezjoseluis@gmail.com>
2021-04-02 23:34:09 -0500

    correcion en los permisos editar,eliminar, en las tablas usuarios

--- Config/Config.php
+++ Config/Config.php
@@ -1,15 +1,15 @@
-<?php 
-	
+<?php
 
-/* 	const BASE_URL = "http://localhost:90/Campesinavirtual";
 
-	date_default_timezone_set('America/Guatemala');
-	const DB_HOST = "localhost";
-	const DB_NAME = "db_campesinavirtual";
-	const DB_USER = "root";
-	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8"; */
+const BASE_URL = "http://localhost:90/Campesinavirtual";
 
+date_default_timezone_set('America/Guatemala');
+const DB_HOST = "localhost";
+const DB_NAME = "db_campesinavirtual";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "utf8";
+/* 
 	const BASE_URL = "https://campesinavirtual.herokuapp.com";
 
 	date_default_timezone_set('America/Guatemala');
@@ -19,16 +19,16 @@
 	const DB_USER = "dnjmxhbd9vepxngn";
 	const DB_PASSWORD = "euouo3jyoy0o5vzj";
 	const DB_CHARSET = "utf8";
+ */
+//Deliminadores decimal y millar Ej. 24,1989.00
+const SPD = ".";
+const SPM = ",";
 
-	//Deliminadores decimal y millar Ej. 24,1989.00
-	const SPD = ".";
-	const SPM = ",";
+//Simbolo de moneda
+const SMONEY = "Q";
 
-	//Simbolo de moneda
-	const SMONEY = "Q";
-
-	//Datos envio de correo
-	const NOMBRE_REMITENTE = "Campesina Virtual";
-	const EMAIL_REMITENTE = "no-reply@abelosh.com";
-	const NOMBRE_EMPESA = "Campesina Virtual";
-	const WEB_EMPRESA = "www.abelosh.com";
+//Datos envio de correo
+const NOMBRE_REMITENTE = "Campesina Virtual";
+const EMAIL_REMITENTE = "no-reply@abelosh.com";
+const NOMBRE_EMPESA = "Campesina Virtual";
+const WEB_EMPRESA = "www.abelosh.com";
